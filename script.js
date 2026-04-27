3cb9b69385ffd3015219f017f6e10781023022f1
Kierstin <kierstin.hanifan@gmail.com>
2026-04-27 14:40:04 -0400

    Increase distance between dogs

--- script.js
+++ script.js
@@ -50,8 +50,8 @@ function moveDogs() {
   lunaY += (lTargetY - lunaY) * 0.07;
 
 
-  const aTargetX = clamp(mx - 40, DOG_W, window.innerWidth  - DOG_W);
-  const aTargetY = clamp(my - 25, DOG_H, window.innerHeight - DOG_H - 70);
+  const aTargetX = clamp(mx - 50, DOG_W, window.innerWidth  - DOG_W);
+  const aTargetY = clamp(my - 35, DOG_H, window.innerHeight - DOG_H - 70);
   arloX += (aTargetX - arloX) * 0.05;
   arloY += (aTargetY - arloY) * 0.05;
 

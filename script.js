02f9e5dff1aee4b681325f5d6aa78eecee65d697
Kierstin <kierstin.hanifan@gmail.com>
2026-04-27 14:42:26 -0400

    Increase distance from ball

--- script.js
+++ script.js
@@ -44,14 +44,14 @@ const activeTreats = [];
 
 function moveDogs() {
 
-  const lTargetX = clamp(mx - 35, DOG_W, window.innerWidth  - DOG_W);
-  const lTargetY = clamp(my - 20, DOG_H, window.innerHeight - DOG_H - 70);
+  const lTargetX = clamp(mx - 40, DOG_W, window.innerWidth  - DOG_W);
+  const lTargetY = clamp(my - 25, DOG_H, window.innerHeight - DOG_H - 70);
   lunaX += (lTargetX - lunaX) * 0.07;
   lunaY += (lTargetY - lunaY) * 0.07;
 
 
-  const aTargetX = clamp(mx - 50, DOG_W, window.innerWidth  - DOG_W);
-  const aTargetY = clamp(my - 35, DOG_H, window.innerHeight - DOG_H - 70);
+  const aTargetX = clamp(mx - 60, DOG_W, window.innerWidth  - DOG_W);
+  const aTargetY = clamp(my - 45, DOG_H, window.innerHeight - DOG_H - 70);
   arloX += (aTargetX - arloX) * 0.05;
   arloY += (aTargetY - arloY) * 0.05;
 

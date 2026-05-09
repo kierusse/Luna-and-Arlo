0f0da486e00cfb0c3a32d7ef934f1d49c6e4b417
Kierstin <kierstin.hanifan@gmail.com>
2026-05-08 21:52:37 -0400

    Removed clamp boundary
    Allowed dogs to reach closer to the edge of the screen to retrieve dropped treats

--- script.js
+++ script.js
@@ -44,14 +44,14 @@ const activeTreats = [];
 
 function moveDogs() {
 
-  const lTargetX = clamp(mx - 40, DOG_W, window.innerWidth  - DOG_W);
-  const lTargetY = clamp(my - 25, DOG_H, window.innerHeight - DOG_H - 70);
+  const lTargetX = clamp(mx - 40, DOG_W, window.innerWidth);
+  const lTargetY = clamp(my - 25, DOG_H, window.innerHeight - 70);
   lunaX += (lTargetX - lunaX) * 0.07;
   lunaY += (lTargetY - lunaY) * 0.07;
 
 
-  const aTargetX = clamp(mx - 70, DOG_W, window.innerWidth  - DOG_W);
-  const aTargetY = clamp(my - 55, DOG_H, window.innerHeight - DOG_H - 70);
+  const aTargetX = clamp(mx - 70, DOG_W, window.innerWidth);
+  const aTargetY = clamp(my - 55, DOG_H, window.innerHeight - 70);
   arloX += (aTargetX - arloX) * 0.05;
   arloY += (aTargetY - arloY) * 0.05;
 
